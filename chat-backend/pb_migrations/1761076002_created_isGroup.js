58700d17f57cbfb94682d0e7eed690ccb0551a62
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:12:49 +0530

    Update 1761076002_created_isGroup.js

--- chat-backend/pb_migrations/1761076002_created_isGroup.js
+++ chat-backend/pb_migrations/1761076002_created_isGroup.js
@@ -58,9 +58,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+ return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_4119087633");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
