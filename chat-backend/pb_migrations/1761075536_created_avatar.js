8931d9a6314ea36f4d691061c5395b1d0a0bb733
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:07:29 +0530

    Update 1761075536_created_avatar.js

--- chat-backend/pb_migrations/1761075536_created_avatar.js
+++ chat-backend/pb_migrations/1761075536_created_avatar.js
@@ -68,9 +68,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_560205665");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
