4429166d288d88acdb467bb150f01015ab55e15f
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:18:48 +0530

    Update 1761077393_created_content.js

--- chat-backend/pb_migrations/1761077393_created_content.js
+++ chat-backend/pb_migrations/1761077393_created_content.js
@@ -63,9 +63,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_1872121667");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
