f9f7d12f26ae10b8627a0cb961bfcdc4c2b9e95e
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:08:53 +0530

    Update 1761075747_created_lastSeen.js

--- chat-backend/pb_migrations/1761075747_created_lastSeen.js
+++ chat-backend/pb_migrations/1761075747_created_lastSeen.js
@@ -60,9 +60,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_724121231");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
