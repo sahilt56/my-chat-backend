9b83d2954bc217e6d21d76af727be2e80fc80005
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:09:35 +0530

    Update 1761075807_created_chats.js

--- chat-backend/pb_migrations/1761075807_created_chats.js
+++ chat-backend/pb_migrations/1761075807_created_chats.js
@@ -49,9 +49,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_3861817060");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
