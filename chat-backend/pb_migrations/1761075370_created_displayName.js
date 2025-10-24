48a7bea488ac229a5f86ad89fba4a1d10830e072
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:06:32 +0530

    Update

--- chat-backend/pb_migrations/1761075370_created_displayName.js
+++ chat-backend/pb_migrations/1761075370_created_displayName.js
@@ -63,9 +63,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_2547925228");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
