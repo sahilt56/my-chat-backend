e1a9f0198b16380092fc98d7b36910ba8468b844
sahil <161115424+sahilt56@users.noreply.github.com>
2025-10-24 19:19:31 +0530

    Update 1761077495_created_type.js

--- chat-backend/pb_migrations/1761077495_created_type.js
+++ chat-backend/pb_migrations/1761077495_created_type.js
@@ -65,9 +65,9 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return app.dao().saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_2882744473");
 
-  return app.delete(collection);
+  return app.dao().deleteCollection(collection);
 })
